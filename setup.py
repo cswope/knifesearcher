d229815cbe24251253c2d3b1571f1544fce4e0b7
Swope, Christopher <christopher.swope@capella.edu>
2016-04-16 14:19:55 -0500

    re adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='cputils',
+      version='0.0.2',
+      author='Chris Swope',
+      maintainer='Chris Swope',
+      maintainer_email='cswope@capella.edu',
+      url='https://github.com/cswope/knifesearcher',
+      packages=['src']
+     )
