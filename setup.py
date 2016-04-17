3cac22cffdfe34296a9a5adbee5e66b2a4bbdbd9
Swope, Christopher <christopher.swope@capella.edu>
2016-04-17 09:37:41 -0500

    added requests as a dependency

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
-setup(name='cputils',
+setup(name='knifesearcher',
       version='0.0.2',
       author='Chris Swope',
       maintainer='Chris Swope',
       maintainer_email='cswope@capella.edu',
       url='https://github.com/cswope/knifesearcher',
-      packages=['src']
+      packages=['knifesearcher'],
+      install_requires=['requests'],
      )
